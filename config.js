f32f9f696bb76799673f9fcd44e711d283c758d7
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-05 17:57:42 +0530

    Auto-update backend URL to https://research-drum-nodes-apache.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-05 12:20:47.
+// Auto-updated by the Colab backend on 2026-09-05 12:27:41.
 const CONFIG = {
-  BACKEND_URL: "https://experimental-casa-teacher-ing.trycloudflare.com",
+  BACKEND_URL: "https://research-drum-nodes-apache.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
