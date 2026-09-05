f1040a6f2658293cefcf2d180828f42273a3860f
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-05 15:02:17 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+
+    BACKEND_URL:
+        "https://YOUR-CURRENT-COLAB-URL.trycloudflare.com",
+
+    APP_NAME:
+        "Find Your Photos",
+
+    MAX_UPLOAD_MB:
+        15
+
+};
