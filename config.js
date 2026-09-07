4ae8bc0f49df0c93bb260bd30bab096b6b1abbec
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 10:09:19 +0530

    Auto-update backend URL to https://shirt-effect-monthly-surprise.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-05 12:27:41.
+// Auto-updated by the Colab backend on 2026-09-07 04:39:19.
 const CONFIG = {
-  BACKEND_URL: "https://research-drum-nodes-apache.trycloudflare.com",
+  BACKEND_URL: "https://shirt-effect-monthly-surprise.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
