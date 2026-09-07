ce85e5e091483e80c4f80bbb6c296f98f09a5340
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 10:39:18 +0530

    Auto-update backend URL to https://nose-profession-cant-average.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 05:06:09.
+// Auto-updated by the Colab backend on 2026-09-07 05:09:18.
 const CONFIG = {
-  BACKEND_URL: "https://jackets-occupation-madonna-symptoms.trycloudflare.com",
+  BACKEND_URL: "https://nose-profession-cant-average.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
