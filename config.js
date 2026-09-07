bf3ff787e7d519c902c7058b987abb19c838bdd7
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 16:47:11 +0530

    Auto-update backend URL to https://nutrition-fly-poet-modeling.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 11:11:46.
+// Auto-updated by the Colab backend on 2026-09-07 11:17:10.
 const CONFIG = {
-  BACKEND_URL: "https://cute-level-remark-rev.trycloudflare.com",
+  BACKEND_URL: "https://nutrition-fly-poet-modeling.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
