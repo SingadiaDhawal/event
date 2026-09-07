92fe2285701dd25f8f1d63262303abf17d6fc526
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 15:04:34 +0530

    Auto-update backend URL to https://money-sphere-urge-lenders.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 07:47:22.
+// Auto-updated by the Colab backend on 2026-09-07 09:34:34.
 const CONFIG = {
-  BACKEND_URL: "https://contains-careful-hebrew-cakes.trycloudflare.com",
+  BACKEND_URL: "https://money-sphere-urge-lenders.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
