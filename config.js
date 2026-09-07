8676ef167aea8e38eeaca8482ab005516197a9fc
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 13:17:23 +0530

    Auto-update backend URL to https://contains-careful-hebrew-cakes.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 06:53:00.
+// Auto-updated by the Colab backend on 2026-09-07 07:47:22.
 const CONFIG = {
-  BACKEND_URL: "https://note-personals-thank-calls.trycloudflare.com",
+  BACKEND_URL: "https://contains-careful-hebrew-cakes.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
