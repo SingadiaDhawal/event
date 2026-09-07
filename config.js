8bf3e08dfd9dbe68664415ad28de5776c0378cde
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 12:23:01 +0530

    Auto-update backend URL to https://note-personals-thank-calls.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 06:20:05.
+// Auto-updated by the Colab backend on 2026-09-07 06:53:00.
 const CONFIG = {
-  BACKEND_URL: "https://jelsoft-connector-chronicle-customise.trycloudflare.com",
+  BACKEND_URL: "https://note-personals-thank-calls.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
