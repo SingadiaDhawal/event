c543bf417fe5be795003a7890950e35d198432dc
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 11:50:05 +0530

    Auto-update backend URL to https://jelsoft-connector-chronicle-customise.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 06:18:50.
+// Auto-updated by the Colab backend on 2026-09-07 06:20:05.
 const CONFIG = {
-  BACKEND_URL: "https://environments-throwing-reserves-lean.trycloudflare.com",
+  BACKEND_URL: "https://jelsoft-connector-chronicle-customise.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
