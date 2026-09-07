b1a48fcafe1522082ec365d19932b4fe93b32cd7
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 17:14:23 +0530

    Auto-update backend URL to https://howto-reconstruction-adopted-unions.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 11:20:22.
+// Auto-updated by the Colab backend on 2026-09-07 11:44:23.
 const CONFIG = {
-  BACKEND_URL: "https://door-distributions-mesa-reed.trycloudflare.com",
+  BACKEND_URL: "https://howto-reconstruction-adopted-unions.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
