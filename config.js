ec28026b7ff758bbb4b553d732d43064f425a152
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 16:41:46 +0530

    Auto-update backend URL to https://cute-level-remark-rev.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 10:36:08.
+// Auto-updated by the Colab backend on 2026-09-07 11:11:46.
 const CONFIG = {
-  BACKEND_URL: "https://inspection-carb-concert-morgan.trycloudflare.com",
+  BACKEND_URL: "https://cute-level-remark-rev.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
