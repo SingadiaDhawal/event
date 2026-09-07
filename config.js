006be5c9aea71ad31d78d454d31ab11b168f44be
SingadiaDhawal <dsingadia1@gmail.com>
2026-09-07 16:06:08 +0530

    Auto-update backend URL to https://inspection-carb-concert-morgan.trycloudflare.com

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Auto-updated by the Colab backend on 2026-09-07 09:34:34.
+// Auto-updated by the Colab backend on 2026-09-07 10:36:08.
 const CONFIG = {
-  BACKEND_URL: "https://money-sphere-urge-lenders.trycloudflare.com",
+  BACKEND_URL: "https://inspection-carb-concert-morgan.trycloudflare.com",
   APP_NAME: "Find Your Photos",
   MAX_UPLOAD_MB: 15
 };
